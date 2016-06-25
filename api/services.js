f50f8c508378580fd8d5e0565c380d063cbedad9
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2016-06-25 23:16:24 +0100

    fix natural-language-classifier-standard to natural-language-classifier-service to match create service cmd in readme

--- api/services.js
+++ api/services.js
@@ -26,7 +26,7 @@ var classifierFile = __dirname + '/../training/classifier_id';
 // This application requires 3 ids to work properly
 
 // 1. TMDB API key, explained in https://www.themoviedb.org/documentation/api
-var TMDB_API_KEY = process.env.TMDB_API_KEY || '';
+var TMDB_API_KEY = process.env.TMDB_API_KEY || '74aa6798298c77c05d3b2eefeb1b6147';
 
 // 2. dialog id - see training/setup.js
 var DIALOG_ID = 'TYPE DIALOG ID HERE';
